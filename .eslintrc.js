25b69eef85d0965042bbc6d20a0351f34eb58fe0
laomao800 <laomao800@gmail.com>
2018-12-06 12:23:02 +0800

    chore: eslint 规则调整

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,11 +8,11 @@ module.exports = {
   rules: {
     // Turn off 'space-before-function-paren' for prettier
     // https://github.com/prettier/prettier/issues/3847
-    'space-before-function-paren': 0,
+    'space-before-function-paren': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
     'vue/max-attributes-per-line': [
-      2,
+      'off',
       {
         singleline: 2,
         multiline: {
@@ -21,10 +21,28 @@ module.exports = {
         }
       }
     ],
+    'vue/component-name-in-template-casing': [
+      'error',
+      'kebab-case',
+      {
+        ignores: []
+      }
+    ],
+    'vue/html-closing-bracket-spacing': [
+      'error',
+      {
+        startTag: 'never',
+        endTag: 'never',
+        selfClosingTag: 'always'
+      }
+    ],
     'vue/multiline-html-element-content-newline': 'error',
     'vue/no-spaces-around-equal-signs-in-attribute': 'error',
     // 'vue/script-indent': ['error', 2, { baseIndent: 0 }],
-    'vue/no-unused-components': 0
+    'vue/no-unused-components': 'off'
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
   },
   overrides: [
     {
