8ce23a788748f4150eda8c6bcdc7fe14a224f0b8
laomao800 <laomao800@gmail.com>
2019-05-09 19:38:37 +0800

    chore: 移除 @vue/eslint-config-standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,6 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#bulb-rules
     'plugin:vue/recommended',
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard',
     // https://github.com/prettier/eslint-config-prettier
     'prettier',
     'prettier/standard',
