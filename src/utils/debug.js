9acd7de12ad0571984b5c4fb85adfcd70ab5867a
laomao800-Mac <laomao800@qq.com>
2019-11-14 01:22:27 +0800

    style: 增加控制台提示样式

--- src/utils/debug.js
+++ src/utils/debug.js
@@ -7,11 +7,19 @@ export let error = noop
 if (process.env.NODE_ENV !== 'test') {
   warn = msg => {
     // eslint-disable-next-line no-console
-    console.warn(`[Listview warn] ${msg}`)
+    console.warn(
+      `%c Listview warn %c ${msg}`,
+      'background:#f90;padding:1px;border-radius:3px;color:#fff',
+      'background:transparent'
+    )
   }
 
   error = msg => {
     // eslint-disable-next-line no-console
-    console.error(`[Listview error] ${msg}`)
+    console.error(
+      `%c Listview error %c ${msg}`,
+      'background:#e30;padding:1px;border-radius:3px;color:#fff',
+      'background:transparent'
+    )
   }
 }
