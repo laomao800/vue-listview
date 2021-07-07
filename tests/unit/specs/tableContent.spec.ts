9c0ea05233a4eb248b0cd586e6e2e93c10362518
laomao800 <laomao800@gmail.com>
2021-07-07 12:28:11 +0800

    test: 补充 table selection 用例

---
+++ tests/unit/specs/tableContent.spec.ts
@@ -0,0 +1,53 @@
+import { createListviewWrapper, wait } from '../helpers'
+
+describe('Table content', () => {
+  it('Column render', async () => {
+    const tableColumns = [
+      { label: 'column1' },
+      { label: 'column1' },
+      { label: 'column1' },
+    ]
+    const { wrapper } = await createListviewWrapper({ tableColumns })
+    expect(wrapper.findAllComponents({ name: 'ElTableColumn' }).length).toBe(
+      // 加上选择列
+      tableColumns.length + 1
+    )
+  })
+
+  it('Row select', async () => {
+    const tableColumns = [
+      { label: 'column1' },
+      { label: 'column1' },
+      { label: 'column1' },
+    ]
+    const { wrapper, storeWrapper, storeVm } = await createListviewWrapper({
+      tableColumns,
+    })
+    const rowEl = wrapper
+      .findComponent({ name: 'ElTableBody' })
+      .findAll('.el-table__row')
+    await rowEl.at(1).find('td').element.click()
+    await rowEl.at(2).find('td').element.click()
+    expect(storeWrapper.emitted('update:tableSelection')!.length).toBe(2)
+    expect(storeVm.internalSelection.length).toBe(2)
+  })
+
+  it('Row single select', async () => {
+    const tableColumns = [
+      { label: 'column1' },
+      { label: 'column1' },
+      { label: 'column1' },
+    ]
+    const { wrapper, storeWrapper, storeVm } = await createListviewWrapper({
+      tableColumns,
+      tableSelectionColumn: 'single',
+    })
+    const rowEl = wrapper
+      .findComponent({ name: 'ElTableBody' })
+      .findAll('.el-table__row')
+    await rowEl.at(1).find('td').element.click()
+    await rowEl.at(2).find('td').element.click()
+    expect(storeWrapper.emitted('update:tableSelection')!.length).toBe(2)
+    expect(storeVm.internalSelection.length).toBe(1)
+  })
+})
