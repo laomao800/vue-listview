edbf7d510207944a13cdc70520e68a949443a65e
laomao800 <laomao800@gmail.com>
2021-07-12 17:24:15 +0800

    补充 selectable 相关用例

--- tests/unit/specs/tableContent.spec.ts
+++ tests/unit/specs/tableContent.spec.ts
@@ -23,13 +23,38 @@ describe('Table content', () => {
     const { wrapper, storeWrapper, storeVm } = await createListviewWrapper({
       tableColumns,
     })
+    const rowWrapper = wrapper
+      .findComponent({ name: 'ElTableBody' })
+      .findAll('.el-table__row')
+    await rowWrapper.at(1).find('td').element.click()
+    await rowWrapper.at(2).find('td').element.click()
+    expect(storeWrapper.emitted('update:tableSelection')!.length).toBe(2)
+    expect(storeVm.internalSelection.length).toBe(2)
+    expect(wrapper.findAll('tr.row--selected').length).toBe(2)
+  })
+
+  it('Row selectable', async () => {
+    const tableColumns = [
+      { label: 'column1' },
+      { label: 'column1' },
+      { label: 'column1' },
+    ]
+    const { wrapper, storeWrapper, storeVm } = await createListviewWrapper({
+      tableColumns,
+      tableSelectionColumn: {
+        selectable: (row, index) => index !== 1,
+      },
+    })
     const rowEl = wrapper
       .findComponent({ name: 'ElTableBody' })
       .findAll('.el-table__row')
-    await rowEl.at(1).find('td').element.click()
-    await rowEl.at(2).find('td').element.click()
+    // td 点击事件未触发 el-table @row-click ，此处通过点击 el-ckeckbox 触发
+    await rowEl.at(0).find('td .el-checkbox__original').element.click()
+    await rowEl.at(1).find('td .el-checkbox__original').element.click()
+    await rowEl.at(2).find('td .el-checkbox__original').element.click()
     expect(storeWrapper.emitted('update:tableSelection')!.length).toBe(2)
     expect(storeVm.internalSelection.length).toBe(2)
+    expect(wrapper.findAll('tr.row--selected').length).toBe(2)
   })
 
   it('Row single select', async () => {
@@ -42,12 +67,13 @@ describe('Table content', () => {
       tableColumns,
       tableSelectionColumn: 'single',
     })
-    const rowEl = wrapper
+    const rowWrapper = wrapper
       .findComponent({ name: 'ElTableBody' })
       .findAll('.el-table__row')
-    await rowEl.at(1).find('td').element.click()
-    await rowEl.at(2).find('td').element.click()
+    await rowWrapper.at(1).find('td').element.click()
+    await rowWrapper.at(2).find('td').element.click()
     expect(storeWrapper.emitted('update:tableSelection')!.length).toBe(2)
     expect(storeVm.internalSelection.length).toBe(1)
+    expect(wrapper.findAll('tr.row--selected').length).toBe(1)
   })
 })
