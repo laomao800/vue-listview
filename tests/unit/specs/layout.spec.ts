4a033ef3a1e02696f5bf0c3118d28508debbdbb3
laomao800 <laomao800@gmail.com>
2021-07-13 00:32:22 +0800

    补充 listview container 用例

--- tests/unit/specs/layout.spec.ts
+++ tests/unit/specs/layout.spec.ts
@@ -2,187 +2,220 @@ import { mount } from '@vue/test-utils'
 import { createListviewWrapper, wait } from '../helpers'
 import Listview from '@/Listview'
 import ListviewHeader from '@/components/ListviewHeader.vue'
+import ListviewContainer from '@/ListviewContainer'
 
-describe('layout', () => {
-  describe('height', () => {
-    it('full height', async () => {
-      Object.defineProperty(window, 'innerHeight', {
-        writable: true,
-        configurable: true,
-        value: 800,
-      })
-
-      const { wrapper } = await createListviewWrapper({
-        autoload: false,
-        usePage: false,
-      })
-      expect(wrapper.element.style.height).toBe('800px')
-
-      Object.defineProperty(window, 'innerHeight', {
-        writable: true,
-        configurable: true,
-        value: 500,
-      })
-      window.dispatchEvent(new Event('resize'))
-      await wait()
-      expect(wrapper.element.style.height).toBe('500px')
+describe('height', () => {
+  it('full height', async () => {
+    Object.defineProperty(window, 'innerHeight', {
+      writable: true,
+      configurable: true,
+      value: 800,
     })
 
-    it('auto height', async () => {
-      const { wrapper } = await createListviewWrapper({
-        autoload: false,
-        usePage: false,
-        fullHeight: false,
-      })
-      expect(wrapper.element.style.height).toBe('')
+    const { wrapper } = await createListviewWrapper({
+      autoload: false,
+      usePage: false,
     })
+    expect(wrapper.element.style.height).toBe('800px')
 
-    it('specify height', async () => {
-      const { wrapper } = await createListviewWrapper({
-        autoload: false,
-        usePage: false,
-        height: 500,
-      })
-      expect(wrapper.element.style.height).toBe('500px')
+    Object.defineProperty(window, 'innerHeight', {
+      writable: true,
+      configurable: true,
+      value: 500,
     })
+    window.dispatchEvent(new Event('resize'))
+    await wait()
+    expect(wrapper.element.style.height).toBe('500px')
   })
 
-  describe('ListviewHeader', () => {
-    it('empty', () => {
-      const wrapper = mount(ListviewHeader)
-      expect(wrapper.html()).toMatchSnapshot()
+  it('auto height', async () => {
+    const { wrapper } = await createListviewWrapper({
+      autoload: false,
+      usePage: false,
+      fullHeight: false,
     })
-
-    it('title', () => {
-      const headerTitle = 'headerTitle'
-      const wrapper = mount(ListviewHeader, {
-        propsData: { headerTitle },
-      })
-      expect(wrapper.text()).toMatch(headerTitle)
-    })
-
-    it('nav', async () => {
-      const wrapper = await mount(ListviewHeader, {
-        propsData: {
-          headerNav: ['home', { text: 'list', to: '/list' }],
-        },
-      })
-      expect(wrapper.html()).toMatchSnapshot()
-    })
-
-    it('title & nav', async () => {
-      const wrapper = await mount(ListviewHeader, {
-        propsData: {
-          headerTitle: 'headerTitle',
-          headerNav: [
-            { text: 'home', to: '/home' },
-            { text: 'list', to: '/list' },
-          ],
-        },
-      })
-      expect(wrapper.html()).toMatchSnapshot()
-    })
+    expect(wrapper.element.style.height).toBe('')
   })
 
-  describe('contentMessage', () => {
-    it('string', async () => {
-      const contentMessage = 'message text'
-      const { wrapper } = await createListviewWrapper({
-        autoload: false,
-        contentMessage,
-      })
-      const messageBlock = wrapper.find('.lv__message')
-      expect(messageBlock.find('.el-icon-warning').exists()).toBe(true)
-      expect(messageBlock.find('.lv__message-text').text()).toMatch(
-        contentMessage
-      )
+  it('specify height', async () => {
+    const { wrapper } = await createListviewWrapper({
+      autoload: false,
+      usePage: false,
+      height: 500,
     })
+    expect(wrapper.element.style.height).toBe('500px')
+  })
+})
 
-    it('object', async () => {
-      const contentMessage: any = {
-        type: 'info',
-        text: 'message text',
-      }
-
-      const { wrapper } = await createListviewWrapper({
-        autoload: false,
-        contentMessage,
-      })
-      const messageBlock = wrapper.find('.lv__message')
-      expect(messageBlock.find('.el-icon-info').exists()).toBe(true)
-      expect(messageBlock.find('.lv__message-text').text()).toMatch(
-        contentMessage.text
-      )
-    })
+describe('listview header', () => {
+  it('empty', () => {
+    const wrapper = mount(ListviewHeader)
+    expect(wrapper.html()).toMatchSnapshot()
   })
 
-  describe('row class name', () => {
-    it('row string class', async () => {
-      const { wrapper } = await createListviewWrapper({
-        requestHandler: () => ({
-          result: { items: [{}], total: 1 },
-          is_success: true,
-        }),
-        contentProps: { rowClassName: 'row-view-class' },
-      })
-      expect(
-        wrapper.findAll('.el-table__body-wrapper .el-table__row.row-view-class')
-          .length
-      ).toBe(1)
+  it('title', () => {
+    const headerTitle = 'headerTitle'
+    const wrapper = mount(ListviewHeader, {
+      propsData: { headerTitle },
     })
-
-    it('row func class', async () => {
-      const { wrapper } = await createListviewWrapper({
-        requestHandler: () => ({
-          result: { items: [{}], total: 1 },
-          is_success: true,
-        }),
-        contentProps: {
-          rowClassName: () => 'row-view-class-fn',
-        },
-      })
-      expect(
-        wrapper.findAll(
-          '.el-table__body-wrapper .el-table__row.row-view-class-fn'
-        ).length
-      ).toBe(1)
-    })
+    expect(wrapper.text()).toMatch(headerTitle)
   })
 
-  describe('listview footer', () => {
-    it('pager off', async () => {
-      const { wrapper } = await createListviewWrapper({ usePage: false })
-      expect(wrapper.vm.$el.querySelector('.lv__pager')).toBe(null)
+  it('nav', async () => {
+    const wrapper = await mount(ListviewHeader, {
+      propsData: {
+        headerNav: ['home', { text: 'list', to: '/list' }],
+      },
     })
+    expect(wrapper.html()).toMatchSnapshot()
+  })
 
-    it('pager on left', async () => {
-      const { wrapper } = await createListviewWrapper({ pagePosition: 'left' })
-      expect(wrapper.find('.lv__footer-left .lv__pager').exists()).toBe(true)
-      expect(wrapper.find('.lv__footer-right .lv__pager').exists()).toBe(false)
+  it('title & nav', async () => {
+    const wrapper = await mount(ListviewHeader, {
+      propsData: {
+        headerTitle: 'headerTitle',
+        headerNav: [
+          { text: 'home', to: '/home' },
+          { text: 'list', to: '/list' },
+        ],
+      },
     })
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
 
-    it('pager on right', async () => {
-      const { wrapper } = await createListviewWrapper({
-        pagePosition: 'right',
-      })
-      expect(wrapper.find('.lv__footer-left .lv__pager').exists()).toBe(false)
-      expect(wrapper.find('.lv__footer-right .lv__pager').exists()).toBe(true)
+describe('contentMessage', () => {
+  it('string', async () => {
+    const contentMessage = 'message text'
+    const { wrapper } = await createListviewWrapper({
+      autoload: false,
+      contentMessage,
     })
+    const messageBlock = wrapper.find('.lv__message')
+    expect(messageBlock.find('.el-icon-warning').exists()).toBe(true)
+    expect(messageBlock.find('.lv__message-text').text()).toMatch(
+      contentMessage
+    )
+  })
 
-    it('footer slots', async () => {
-      const slots = {
-        'footer-left': 'footer left',
-        'footer-center': 'footer center',
-        'footer-right': 'footer right',
-      }
-      const wrapper = await mount(Listview, { slots })
-      expect(wrapper.find('.lv__footer-left').text()).toBe(slots['footer-left'])
-      expect(wrapper.find('.lv__footer-center').text()).toBe(
-        slots['footer-center']
-      )
-      expect(wrapper.find('.lv__footer-right').text()).toBe(
-        slots['footer-right']
-      )
+  it('object', async () => {
+    const contentMessage: any = {
+      type: 'info',
+      text: 'message text',
+    }
+
+    const { wrapper } = await createListviewWrapper({
+      autoload: false,
+      contentMessage,
     })
+    const messageBlock = wrapper.find('.lv__message')
+    expect(messageBlock.find('.el-icon-info').exists()).toBe(true)
+    expect(messageBlock.find('.lv__message-text').text()).toMatch(
+      contentMessage.text
+    )
+  })
+})
+
+describe('row class name', () => {
+  it('row string class', async () => {
+    const { wrapper } = await createListviewWrapper({
+      requestHandler: () => ({
+        result: { items: [{}], total: 1 },
+        is_success: true,
+      }),
+      contentProps: { rowClassName: 'row-view-class' },
+    })
+    expect(
+      wrapper.findAll('.el-table__body-wrapper .el-table__row.row-view-class')
+        .length
+    ).toBe(1)
+  })
+
+  it('row func class', async () => {
+    const { wrapper } = await createListviewWrapper({
+      requestHandler: () => ({
+        result: { items: [{}], total: 1 },
+        is_success: true,
+      }),
+      contentProps: {
+        rowClassName: () => 'row-view-class-fn',
+      },
+    })
+    expect(
+      wrapper.findAll(
+        '.el-table__body-wrapper .el-table__row.row-view-class-fn'
+      ).length
+    ).toBe(1)
+  })
+})
+
+describe('listview footer', () => {
+  it('pager off', async () => {
+    const { wrapper } = await createListviewWrapper({ usePage: false })
+    expect(wrapper.vm.$el.querySelector('.lv__pager')).toBe(null)
+  })
+
+  it('pager on left', async () => {
+    const { wrapper } = await createListviewWrapper({ pagePosition: 'left' })
+    expect(wrapper.find('.lv__footer-left .lv__pager').exists()).toBe(true)
+    expect(wrapper.find('.lv__footer-right .lv__pager').exists()).toBe(false)
+  })
+
+  it('pager on right', async () => {
+    const { wrapper } = await createListviewWrapper({
+      pagePosition: 'right',
+    })
+    expect(wrapper.find('.lv__footer-left .lv__pager').exists()).toBe(false)
+    expect(wrapper.find('.lv__footer-right .lv__pager').exists()).toBe(true)
+  })
+
+  it('footer slots', async () => {
+    const slots = {
+      'footer-left': 'footer left',
+      'footer-center': 'footer center',
+      'footer-right': 'footer right',
+    }
+    const wrapper = await mount(Listview, { slots })
+    expect(wrapper.find('.lv__footer-left').text()).toBe(slots['footer-left'])
+    expect(wrapper.find('.lv__footer-center').text()).toBe(
+      slots['footer-center']
+    )
+    expect(wrapper.find('.lv__footer-right').text()).toBe(slots['footer-right'])
+  })
+})
+
+describe('listview container', () => {
+  it('header title', async () => {
+    const wrapper = await mount({
+      components: { ListviewContainer, Listview },
+      template:
+        '<ListviewContainer>' +
+        '<Listview header-title="title1" />' +
+        '<Listview headerTitle="title2" />' +
+        '</ListviewContainer>',
+    })
+    expect(wrapper.findAll('div.lvc__tab').length).toBe(2)
+    expect(wrapper.findAll('div.lvc__tab').at(0).element.textContent).toBe(
+      'title1'
+    )
+    expect(wrapper.findAll('div.lvc__tab').at(1).element.textContent).toBe(
+      'title2'
+    )
+  })
+
+  it('tab content switch', async () => {
+    const wrapper = await mount({
+      components: { ListviewContainer, Listview },
+      template:
+        '<ListviewContainer>' +
+        '<div class="content1" header-title="title1" />' +
+        '<Listview header-title="title2" />' +
+        '</ListviewContainer>',
+    })
+    expect(wrapper.find('div.content1').exists()).toBe(true)
+    expect(wrapper.find('div.lv_wrapper').exists()).toBe(false)
+    await wrapper.findAll('div.lvc__tab').at(1).element.click()
+    expect(wrapper.find('div.content1').exists()).toBe(false)
+    expect(wrapper.find('div.lv__wrapper').exists()).toBe(true)
   })
 })
