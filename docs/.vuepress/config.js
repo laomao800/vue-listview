25a92ec285fa138fb63f14d9ffb2c477fc441886
laomao800 <laomao800@gmail.com>
2021-07-21 17:58:14 +0800

    修正文档部署路径配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,10 @@ const path = require('path')
 const version =
   process.env.TARGET_VERSION || require('../../package.json').version
 
-const base = process.env.NODE_ENV === 'production' ? '/vue-listview/' : ''
+const base =
+  process.env.NODE_ENV === 'production'
+    ? `/vue-listview${process.env.TARGET_VERSION_PATH || '/'}`
+    : ''
 
 module.exports = {
   base,
