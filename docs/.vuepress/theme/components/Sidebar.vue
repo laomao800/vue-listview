b0674b1c3def043f0c235deea170307d95ac8ece
laomao800 <laomao800@gmail.com>
2021-07-22 16:42:22 +0800

    doc: 补充文档版本切换功能

---
+++ docs/.vuepress/theme/components/Sidebar.vue
@@ -0,0 +1,20 @@
+<template>
+  <OriginSidebar :items="$attrs.items">
+    <template #top>
+      <VersionSwitch />
+    </template>
+  </OriginSidebar>
+</template>
+
+<script>
+import OriginSidebar from '@parent-theme/components/Sidebar.vue'
+import VersionSwitch from './VersionSwitch.vue'
+
+export default {
+  name: 'NewSidebar',
+  components: {
+    OriginSidebar,
+    VersionSwitch,
+  },
+}
+</script>
